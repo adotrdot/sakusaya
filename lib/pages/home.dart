e2856c19c24bce6d4553d4a7b8d04f718ea020b8
Alvin Aryanta S. <a.aryanta.s@gmail.com>
2024-04-13 22:54:51 +0700

    Test read & write from database with Hive

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class SksyHome extends StatefulWidget {
   const SksyHome({super.key});
@@ -10,6 +11,7 @@ class SksyHome extends StatefulWidget {
 
 class _SksyHomeState extends State<SksyHome> {
   var box = Hive.box('pocket');
+  final totalMoneyController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +26,20 @@ class _SksyHomeState extends State<SksyHome> {
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            Text('Pocket Money: ${box.get('totalMoney', defaultValue: 0)}'),
+            Row(
+              children: [
+                ValueListenableBuilder(
+                  valueListenable: box.listenable(keys: ['totalMoney']),
+                  builder: (context, box, widget) {
+                    return Text('Pocket Money: ${box.get('totalMoney', defaultValue: 0)}');
+                  }
+                ),
+                IconButton(
+                  onPressed: () {dialogBuilder(context);},
+                  icon: const Icon(Icons.edit),
+                ),
+              ],
+            ),
             const Text('Allocation'),
             Text('Food & Drink: ${box.get('foodDrink', defaultValue: 0)}'),
             Text('Transportation: ${box.get('transport', defaultValue: 0)}'),
@@ -38,6 +53,42 @@ class _SksyHomeState extends State<SksyHome> {
           ],
         ),
       ),
+    );
+  }
+
+  Future<void> dialogBuilder(BuildContext context) {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Pocket Money'),
+          content: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: TextField(
+              controller: totalMoneyController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Enter amount',
+              ),
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                int newValue = 0;
+                try {
+                  newValue = int.parse(totalMoneyController.text);
+                } catch(e) {
+                  newValue = 0;
+                }
+                box.put('totalMoney', newValue);
+                Navigator.of(context).pop();
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
