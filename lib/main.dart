7f5a66c8d36f15e13329f685192e253b27fbc063
Alvin Aryanta S. <a.aryanta.s@gmail.com>
2024-04-24 11:14:26 +0700

    Hide debug banner when in debug mode

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,7 @@ class _MyAppState extends State<MyApp> {
               useMaterial3: true,
             ),
             routerConfig: goRouter,
+            debugShowCheckedModeBanner: false,
           );
         } else {
           return const Center(
